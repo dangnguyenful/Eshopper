ad70b77377dec3d5b41bff108572ea295a27516d
Win (Nguyen Truong) <nguyentd@smartosc.com>
2018-04-28 18:01:57 +0700

    Redirect 404

--- src/components/Main/Index.js
+++ src/components/Main/Index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect, DefaultRoute } from 'react-router-dom'
 import Home from '../Page/Index'
 import Login from '../Page/Login'
+import NotFound from '../Page/NotFound'
 const Index = () => (
 	<main>
 		<Switch>
 			<Route exact path='/' component={Home}/>
-			<Route exact path='/login' component={Login}/>
+			<Route path='/login' component={Login}/>
+			<Route path='/not-found' component={NotFound}/>
+			<Redirect from={'/'} to={'/not-found'} />
 		</Switch>
 	</main>
 );
