67deeaaa22251566696958f4994c56560768c7fa
Nguyen Truong <nguyentd@smartosc.com>
2018-05-09 16:15:26 +0700

    Use class name instead

--- src/components/Page/Contact.js
+++ src/components/Page/Contact.js
@@ -1,41 +1,41 @@
 import React from 'react'
 const Contact = () => (
-	<div id="contact-page" class="container">
-    	<div class="bg">
-	    	<div class="row">    		
-	    		<div class="col-sm-12">    			   			
-					<h2 class="title text-center">Contact <strong>Us</strong></h2>    			    				    				
-					<div id="gmap" class="contact-map">
+	<div id="contact-page" className="container">
+    	<div className="bg">
+	    	<div className="row">    		
+	    		<div className="col-sm-12">    			   			
+					<h2 className="title text-center">Contact <strong>Us</strong></h2>    			    				    				
+					<div id="gmap" className="contact-map">
 					</div>
 				</div>			 		
 			</div>    	
-    		<div class="row">  	
-	    		<div class="col-sm-8">
-	    			<div class="contact-form">
-	    				<h2 class="title text-center">Get In Touch</h2>
-	    				{/*<div class="status alert alert-success"></div>*/}
-				    	<form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
-				            <div class="form-group col-md-6">
-				                <input type="text" name="name" class="form-control" required="required" placeholder="Name" />
+    		<div className="row">  	
+	    		<div className="col-sm-8">
+	    			<div className="contact-form">
+	    				<h2 className="title text-center">Get In Touch</h2>
+	    				{/*<div className="status alert alert-success"></div>*/}
+				    	<form id="main-contact-form" className="contact-form row" name="contact-form" method="post">
+				            <div className="form-group col-md-6">
+				                <input type="text" name="name" className="form-control" required="required" placeholder="Name" />
 				            </div>
-				            <div class="form-group col-md-6">
-				                <input type="email" name="email" class="form-control" required="required" placeholder="Email" />
+				            <div className="form-group col-md-6">
+				                <input type="email" name="email" className="form-control" required="required" placeholder="Email" />
 				            </div>
-				            <div class="form-group col-md-12">
-				                <input type="text" name="subject" class="form-control" required="required" placeholder="Subject" />
+				            <div className="form-group col-md-12">
+				                <input type="text" name="subject" className="form-control" required="required" placeholder="Subject" />
 				            </div>
-				            <div class="form-group col-md-12">
-				                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your Message Here"></textarea>
+				            <div className="form-group col-md-12">
+				                <textarea name="message" id="message" required="required" className="form-control" rows="8" placeholder="Your Message Here"></textarea>
 				            </div>                        
-				            <div class="form-group col-md-12">
-				                <input type="submit" name="submit" class="btn btn-primary pull-right" value="Submit" />
+				            <div className="form-group col-md-12">
+				                <input type="submit" name="submit" className="btn btn-primary pull-right" value="Submit" />
 				            </div>
 				        </form>
 	    			</div>
 	    		</div>
-	    		<div class="col-sm-4">
-	    			<div class="contact-info">
-	    				<h2 class="title text-center">Contact Info</h2>
+	    		<div className="col-sm-4">
+	    			<div className="contact-info">
+	    				<h2 className="title text-center">Contact Info</h2>
 	    				<address>
 	    					<p>E-Shopper Inc.</p>
 							<p>935 W. Webster Ave New Streets Chicago, IL 60614, NY</p>
@@ -44,20 +44,20 @@ const Contact = () => (
 							<p>Fax: 1-714-252-0026</p>
 							<p>Email: info@e-shopper.com</p>
 	    				</address>
-	    				<div class="social-networks">
-	    					<h2 class="title text-center">Social Networking</h2>
+	    				<div className="social-networks">
+	    					<h2 className="title text-center">Social Networking</h2>
 							<ul>
 								<li>
-									<a href="/"><i class="fa fa-facebook"></i></a>
+									<a href="/"><i className="fa fa-facebook"></i></a>
 								</li>
 								<li>
-									<a href="/"><i class="fa fa-twitter"></i></a>
+									<a href="/"><i className="fa fa-twitter"></i></a>
 								</li>
 								<li>
-									<a href="/"><i class="fa fa-google-plus"></i></a>
+									<a href="/"><i className="fa fa-google-plus"></i></a>
 								</li>
 								<li>
-									<a href="/"><i class="fa fa-youtube"></i></a>
+									<a href="/"><i className="fa fa-youtube"></i></a>
 								</li>
 							</ul>
 	    				</div>
