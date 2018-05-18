60e7fa7d60499c8abacd6abb02dc9adfdf2ce36f
Nguyen Truong <nguyentd@smartosc.com>
2018-05-18 14:17:29 +0700

    Remove unecessary comment

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -10,13 +10,6 @@ const initialState = {
 const getAllProducts = function(state = initialState, action) {
 	switch (action.type) {
 		case ActionTypes.RECEIVE_PRODUCTS:
-			// console.log('---')
-			// console.log({
-			// 	products: action.products,
-			// 	currentPage: action.currentPage,
-			// 	itemPerPage: action.itemPerPage
-			// })
-			// console.log('---')
 			return {
 				products: action.products,
 				currentPage: action.currentPage,
