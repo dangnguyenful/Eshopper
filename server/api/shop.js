41e3c1b0a03ee54ecffe6f3f4f2f01fa89663230
Nguyen Truong <nguyentd@smartosc.com>
2018-05-24 11:36:13 +0700

    Use query function instead

--- server/api/shop.js
+++ server/api/shop.js
@@ -1,20 +1,18 @@
 const _products = require('./products.json');
 
-const getProducts = function (categoryId, callbackFunc) {
-	return callbackFunc({
-		products: _products.filter(function (product){
-			return product.categoryId === parseInt(categoryId);
-		})
-	});
-}
-
-const getAllProducts = function (callbackFunc) {
+const getProducts = function (queryObject, callbackFunc) {
+	const categoryId = queryObject.categoryId;
+	if (categoryId)
+		return callbackFunc({
+			products: _products.filter(function (product){
+				return product.categoryId === parseInt(categoryId);
+			})
+		});
 	return callbackFunc({
 		products: _products
 	});
 }
 
 module.exports = {
-	getProducts,
-	getAllProducts
+	getProducts
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,16 +6,8 @@ const appRouter = function (app) {
 	  	res.status(200).json({ message: 'Welcome to API page !' });
 	});
 
-	app.get("/products/:categoryId", (req, res) => {
-		shop.getProducts(req.params.categoryId, function (data) {
-			if (!data)
-				res.status(404).json(configs.errorNotFound);
-			res.status(200).json(data);
-		})
-	});
-
 	app.get("/products", (req, res) => {
-		shop.getAllProducts(function (data) {
+		shop.getProducts(req.query, function (data) {
 			if (!data)
 				res.status(404).json(configs.errorNotFound);
 			res.status(200).json(data);
