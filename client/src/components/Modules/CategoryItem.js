c2eebbc6a6aa82d264dfa24990a756c3f881b290
Nguyen Truong <nguyentd@smartosc.com>
2018-05-29 18:43:09 +0700

    Click event category

--- client/src/components/Modules/CategoryItem.js
+++ client/src/components/Modules/CategoryItem.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { getProducts } from '../../actions/products'
+import store from '../../store'
 
-const CategoryItem = ({ title }) => {
+const CategoryItem = ({ id, title }) => {
 	return (
 		<div className="panel panel-default">
 			<div className="panel-heading">
-				<h4 className="panel-title"><a href="/">{title}</a></h4>
+				<h4 className="panel-title"><a href="javascript:void(0)" onClick={() => store.dispatch(getProducts({categoryId:id}))}>{title}</a></h4>
 			</div>
 		</div>
 	)
 }
 
 CategoryItem.propTypes = {
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired
 }
 
